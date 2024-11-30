0497316ec35da6ea1bc6715fe6da67c7d35ff112
pgm-stijwalr <147693656+pgm-stijwalr@users.noreply.github.com>
2024-11-30 17:16:19 +0100

    addons

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,13 +1,50 @@
 /** @type { import('@storybook/react').Preview } */
+import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
+import path from "path"; // Import the 'path' module
+
 const preview = {
-  parameters: {
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
+    parameters: {
+        viewport: {
+            viewports: INITIAL_VIEWPORTS,
+            defaultViewport: "ipad",
+        },
+        backgrounds: {
+            values: [
+                // 👇 Default values
+                { name: "Dark", value: "#333" },
+                { name: "Light", value: "#F7F9F2" },
+                // 👇 Add your own
+                { name: "Maroon", value: "#400" },
+            ],
+            // 👇 Specify which background is shown by default
+            default: "Light",
+        },
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/i,
+            },
+        },
+        addons: [
+            {
+                name: "@storybook/addon-storysource",
+                options: {
+                    rule: {
+                        // test: [/\.stories\.jsx?$/], This is default
+                        include: [
+                            path.resolve(
+                                __dirname,
+                                "../src/stories/**/*.stories.js"
+                            ),
+                        ],
+                    },
+                    loaderOptions: {
+                        prettierConfig: { printWidth: 80, singleQuote: false },
+                    },
+                },
+            },
+        ],
     },
-  },
 };
 
 export default preview;
